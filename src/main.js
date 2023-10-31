6343754fce1dd492e58d391d00ef689532431317
Marcus <lunzenauer@elan-ev.de>
2023-10-31 13:30:59 +0100

    Add pinia-plugin-persistedstate

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import { createApp } from 'vue';
 import { createGettext } from 'vue3-gettext';
 import App from './App.vue';
@@ -8,8 +9,10 @@ import './assets/main.css';
 
 const mountLernkarten = (el, data) => {
     const app = createApp(App);
+    const pinia = createPinia();
+    pinia.use(piniaPluginPersistedstate)
 
-    app.use(createPinia());
+    app.use(pinia);
     app.use(router);
     app.use(createGettext({ translations }));
 
