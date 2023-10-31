1d64bee3ba749aad5f183cfd236a2699705db2bb
Marcus <lunzenauer@elan-ev.de>
2023-10-31 13:31:35 +0100

    Add vue sidebar navigation.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <script setup>
+import { onMounted } from 'vue';
 import { RouterLink, RouterView } from 'vue-router';
+import Sidebar from './components/Sidebar.vue';
+
+onMounted(hideOriginalSidebarNavigation);
+
+function hideOriginalSidebarNavigation() {
+    const navigation = document.querySelector('#sidebar-navigation form');
+    navigation.querySelector(
+        '.sidebar-navigation:not(.lernkarten-sidebar-navigation)'
+    ).hidden = true;
+}
 </script>
 
 <template>
@@ -12,4 +23,8 @@ import { RouterLink, RouterView } from 'vue-router';
     </header>
 
     <RouterView />
+
+    <Teleport to="#sidebar-navigation form">
+        <Sidebar />
+    </Teleport>
 </template>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const matchedName = computed(() => route?.matched?.[0]?.name ?? '');
+</script>
+<template>
+    <ul
+        class="widget-list widget-links sidebar-navigation navigation-level-3"
+        :aria-label="$gettext('Dritte Navigationsebene')"
+    >
+        <li :class="{ active: matchedName === 'home' }">
+            <RouterLink
+                to="/"
+                id="nav_lernkarten_index"
+                activeClass="active"
+                ariaCurrentValue="page"
+            >
+                {{ $gettext('Lernkarten') }}
+            </RouterLink>
+        </li>
+        <li :class="{ active: matchedName === 'folders' }">
+            <RouterLink
+                to="/folders"
+                id="nav_lernkarten_folders"
+                activeClass="active"
+                ariaCurrentValue="page"
+            >
+                {{ $gettext('Ordnerverwaltung') }}
+            </RouterLink>
+        </li>
+    </ul>
+</template>
