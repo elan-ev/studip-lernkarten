10aec40f0e696205ab3bdf2762a893e87a5af145
Marcus <lunzenauer@elan-ev.de>
2023-11-10 13:29:36 +0100

    Add CardList(Item) components.

---
+++ src/components/CardList.vue
@@ -0,0 +1,22 @@
+<script setup>
+import CardListItem from './CardListItem.vue';
+
+const props = defineProps(['cards']);
+const emit = defineEmits(['select']);
+
+const onSelect = (card) => emit('select', card);
+</script>
+<template>
+    <article class="studip">
+        <header>
+            <h1>Card list</h1>
+        </header>
+        <CardListItem
+            v-for="(card, index) in cards"
+            :key="card.id"
+            :card="card"
+            :index="index"
+            @select="() => onSelect(card)"
+        />
+    </article>
+</template>
---
+++ src/components/CardListItem.vue
@@ -0,0 +1,29 @@
+<script setup>
+const props = defineProps(['card', 'index']);
+const emit = defineEmits(['select']);
+</script>
+
+<template>
+    <div class="lernkarten-card-list-item" @click="emit('select')">
+        <div>({{ index }}) F: {{ card.fields.front }}</div>
+        <div>A: {{ card.fields.back }}</div>
+    </div>
+</template>
+
+<style>
+.lernkarten-card-list-item {
+    display: flex;
+    cursor: pointer;
+    border: 1px solid var(--light-gray-color-20);
+}
+.lernkarten-card-list-item + .lernkarten-card-list-item {
+    margin-block-start: 0.5rem;
+}
+.lernkarten-card-list-item > div {
+    flex-basis: 50%;
+    padding: 0.5rem;
+}
+.lernkarten-card-list-item > div:last-child {
+    border-left: 1px solid var(--light-gray-color-20);
+}
+</style>
