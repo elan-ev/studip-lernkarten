2e0d7ef29e7dabb2f6c4114cad731780f171d443
Till Glöggler <till@gundk.it>
2023-11-28 09:59:06 +0100

    add missing table field

--- migrations/02_create_decks_table.php
+++ migrations/02_create_decks_table.php
@@ -20,6 +20,7 @@ class CreateDecksTable extends Migration
               `context_id` char(32) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
               `context_type` varchar(300) NOT NULL,
               `shared_deck_id` int(11) UNSIGNED NULL,
+              `template_id` int(11) UNSIGNED,
               `mkdate` int(11) NOT NULL,
               `chdate` int(11) NOT NULL,
               PRIMARY KEY (`id`),
