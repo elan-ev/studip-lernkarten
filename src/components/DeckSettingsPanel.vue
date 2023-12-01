8c2e3437beccca3f81a549619a1a225428aeb694
Marcus <lunzenauer@elan-ev.de>
2023-12-01 13:57:41 +0100

    Add export and prepare import.

---
+++ src/components/DeckSettingsPanel.vue
@@ -0,0 +1,91 @@
+<script setup>
+import { computed } from 'vue';
+import IconButton from './IconButton.vue';
+import { useCardsStore } from '../stores/cards.js';
+
+const cardsStore = useCardsStore();
+
+const props = defineProps(['deck']);
+
+const cards = computed(() => cardsStore.byDeck(props.deck));
+
+const mostRecentCard = computed(() =>
+    cards.value.reduce((memo, card) => {
+        return !memo || new Date(card.chdate) > new Date(memo.chdate) ? card : memo;
+    }, null)
+);
+
+const chdate = computed(() => {
+    if (!mostRecentCard.value) {
+        return props.deck.chdate;
+    }
+
+    return new Date(mostRecentCard.value.chdate) > new Date(props.deck.chdate)
+        ? mostRecentCard.value.chdate
+        : props.deck.chdate;
+});
+
+const onImport = () => {
+    console.debug('onImport');
+};
+const onExport = () => {
+    const data = cards.value
+        .map((card) => {
+            switch (card.model) {
+                case 'basic':
+                    return [card.fields.front, card.fields.back];
+                default:
+                    console.error('Could not export card', card);
+                    return null;
+            }
+        })
+        .filter(Boolean)
+        .map((row) => row.map((v) => `"${String(v).replaceAll('"', '""')}"`).join(','))
+        .join('\r\n');
+
+    const filename = `kartensatz-${props.deck.name}-${chdate.value}.csv`.replace(
+        /[\/|\\:*?"<>]/g,
+        ''
+    );
+
+    download(filename, data);
+};
+
+function download(filename, data) {
+    const blob = new Blob([data], { type: 'text/csv' });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.setAttribute('href', url);
+    a.setAttribute('download', filename);
+    a.click();
+}
+</script>
+
+<template>
+    <article class="studip">
+        <header>
+            <h1>{{ $gettext('Importieren') }}</h1>
+        </header>
+        <section>
+            <p>
+                {{ $gettext('Importiere Karten aus einer CSV-Datei.') }}
+            </p>
+            <IconButton icon="import" type="button" @click="onImport">
+                {{ $gettext('Importieren') }}
+            </IconButton>
+        </section>
+    </article>
+    <article class="studip">
+        <header>
+            <h1>{{ $gettext('Exportieren') }}</h1>
+        </header>
+        <section>
+            <p>
+                {{ $gettext('Exportiere Karten in eine CSV-Datei') }}
+            </p>
+            <IconButton icon="export" type="button" @click="onExport">
+                {{ $gettext('Exportieren') }}
+            </IconButton>
+        </section>
+    </article>
+</template>
--- src/views/DeckView.vue
+++ src/views/DeckView.vue
@@ -5,6 +5,7 @@ import { TabGroup, TabList, Tab, TabPanels, TabPanel } from '@headlessui/vue';
 import Button from '../components/IconButton.vue';
 import DeckCardsPanel from '../components/DeckCardsPanel.vue';
 import DeckInfoPanel from '../components/DeckInfoPanel.vue';
+import DeckSettingsPanel from '../components/DeckSettingsPanel.vue';
 import DeckStatisticsPanel from '../components/DeckStatisticsPanel.vue';
 import DialogAdjustLearningOptions from '../components/DialogAdjustLearningOptions.vue';
 import DialogShareDeck from '../components/DialogShareDeck.vue';
@@ -116,7 +117,9 @@ const onShowShareDialog = () => {
                 <TabPanel>
                     <DeckStatisticsPanel :deck="deck" />
                 </TabPanel>
-                <TabPanel>Content 4 (Einstellungen)</TabPanel>
+                <TabPanel>
+                    <DeckSettingsPanel :deck="deck" />
+                </TabPanel>
             </TabPanels>
         </TabGroup>
     </div>
