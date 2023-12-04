6a5015a43a989e07f5afbf8a7fe39ddce9c56d01
tgloeggl <tgloeggl@uos.de>
2023-12-04 16:54:44 +0100

    add simple, basic, search for decks, refs #30

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,20 +1,52 @@
 <script setup>
+import { toRaw, watch } from 'vue';
+import { useRoute } from 'vue-router';
+import IconButton from '../components/IconButton.vue';
+import StudipIcon from '../components/base/StudipIcon.vue';
+import DeckList from '../components/DeckList.vue';
+
+
 import { computed, ref } from 'vue';
 import { useDecksStore } from '../stores/decks.js';
 
 const decksStore = useDecksStore();
-decksStore.fetchContext();
+decksStore.fetchContext().then(() => {
+    doSearch();
+});
 
 const props = defineProps(['query']);
 
 const query = ref(props.query);
 
 const allDecks = computed(() => decksStore.byContext);
+const filteredDecks = ref([]);
+
+const doSearch = () => {
+    filteredDecks.value = toRaw(allDecks.value.filter((el) => {
+        if (el.name.search(query.value) != -1
+            || el.description.search(query.value) != -1
+        ) {
+            return true;
+        }
+
+        return false;
+    }));
+}
+
+const route = useRoute();
+
+watch(
+    () => route.query,
+    async newQuery => {
+        query.value = newQuery.q;
+        doSearch();
+    }
+);
 </script>
 
 <template>
     <main>
-        <form class="default tw-bg-[var(--content-color-20)] tw-p-3" @submit.prevent="">
+        <form class="default tw-bg-[var(--content-color-20)] tw-p-3 tw-mb-2" @submit.prevent="doSearch">
             <div class="tw-font-bold">
                 <StudipIcon shape="dialog-cards" role="info" class="tw-align-middle" />
                 {{ $gettext('Deine Kartensätze') }}
@@ -39,10 +71,12 @@ const allDecks = computed(() => decksStore.byContext);
                     </label>
                 </div>
 
-                <IconButton icon="search">
+                <IconButton icon="search" :disabled="decksStore.isLoading">
                     {{ $gettext('Nach Kartensätzen suchen') }}
                 </IconButton>
             </section>
         </form>
+
+        <DeckList :decks="filteredDecks" />
     </main>
 </template>
