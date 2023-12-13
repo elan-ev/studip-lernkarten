9f4179c5e08b3bcb0b353e13222a351853a0a67f
Marcus <lunzenauer@elan-ev.de>
2023-12-13 13:55:47 +0100

    Add sidebar action to share a deck into the current context.

--- src/components/SidebarActions.vue
+++ src/components/SidebarActions.vue
@@ -1,30 +1,64 @@
 <script setup>
-import { ref } from 'vue';
-import { RouterLink, useRouter } from 'vue-router';
+import { computed, ref } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import { useGettext } from 'vue3-gettext';
+import { useContextStore } from '../stores/context.js';
+import DialogShareDeckHere from './DialogShareDeckHere.vue';
 import StudipIcon from './base/StudipIcon.vue';
 
+const contextStore = useContextStore();
+const { $gettext } = useGettext();
 const router = useRouter();
+const route = useRoute();
+
+const showShareDialog = ref(false);
+
+const isTeacher = computed(() => contextStore.isCourse && contextStore.isTeacher);
+const isWorkplace = computed(() => !contextStore.isCourse);
 
 const onCreateDeck = () => {
-    const f = 17;
+    const f = route.name === 'folder' ? route.params.id : null;
     router.push({ name: 'decks-create', query: { f } });
 };
+
+const onShareDeck = () => (showShareDialog.value = true);
+
+const actionList = computed(() => {
+    const actions = [];
+    if (isWorkplace.value) {
+        actions.push({
+            icon: 'add',
+            text: $gettext('Neuen Kartensatz erstellen'),
+            click: onCreateDeck
+        });
+    }
+    if (isTeacher.value) {
+        actions.push({
+            icon: 'share',
+            text: $gettext('Kartensatz hierhin teilen'),
+            click: onShareDeck
+        });
+    }
+
+    return actions;
+});
 </script>
 
 <template>
-    <div id="sidebar-actions" class="sidebar-widget">
+    <div v-if="actionList.length" id="sidebar-actions" class="sidebar-widget">
         <div class="sidebar-widget-header">{{ $gettext('Aktionen') }}</div>
         <div class="sidebar-widget-content">
             <form method="post">
                 <ul class="widget-list widget-links" :aria-label="$gettext('Aktionen')">
-                    <li class="!tw-pl-0">
-                        <button type="button" @click="onCreateDeck">
-                            <StudipIcon shape="add" class="!tw-align-middle" />
-                            {{ $gettext('Neuen Kartensatz erstellen') }}
+                    <li v-for="action in actionList" class="!tw-pl-0">
+                        <button type="button" @click="action.click">
+                            <StudipIcon :shape="action.icon" class="!tw-align-middle" />
+                            {{ action.text }}
                         </button>
                     </li>
                 </ul>
             </form>
         </div>
     </div>
+    <DialogShareDeckHere v-if="showShareDialog" v-model:open="showShareDialog" />
 </template>
